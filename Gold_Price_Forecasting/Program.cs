3b9304c66fb713d60440738a05c91617579090bc
Nguyễn Thành Trung <trungff07@gmail.com>
2026-03-03 18:32:36 +0700

    refactor: rename bitcoin artifacts to gold and fix model path

---
+++ Gold_Price_Forecasting/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BitCoin_Price_Forecasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var modelOutput = BitcoinForecastingModel.Predict();
+
+            Console.Write("Nhap so gio de du doan: ");
+
+            int horizion = int.Parse(Console.ReadLine() ?? "5");
+
+            // predict next 5 periods
+            modelOutput = BitcoinForecastingModel.Predict(horizon: horizion);
+            Console.WriteLine(string.Join("\n", modelOutput.Close));
+
+            Console.WriteLine("This is group 3");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
